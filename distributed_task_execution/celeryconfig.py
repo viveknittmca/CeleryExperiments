314a67aacb2791394136d5b9b71eda7adbd11366
Vivek Rajendran <viveknittmca@gmail.com>
2025-02-04 01:16:28 +0530

    AMQPS & RDS with schema

--- distributed_task_execution/celeryconfig.py
+++ distributed_task_execution/celeryconfig.py
@@ -25,3 +25,17 @@ result_backend = "db+postgresql://user:password@localhost/celery_db"
 
 # Followed by
 # celery -A tasks backend_cleanup
+
+broker_url = "amqps://username:password@your-rabbitmq-endpoint.amazonaws.com:5671"
+
+result_backend = "db+postgresql://db_user:db_password@your-rds-endpoint.amazonaws.com:5432/risk"
+
+# Define schema for Celery task results in PostgreSQL
+result_backend_transport_options = {
+    "options": "-c search_path=devops"
+}
+
+# Additional Celery settings
+task_acks_late = True  # Ensure tasks are acknowledged only after execution
+task_reject_on_worker_lost = True  # Prevents tasks from being lost
+worker_prefetch_multiplier = 1  # Ensures tasks are executed sequentially per worker
